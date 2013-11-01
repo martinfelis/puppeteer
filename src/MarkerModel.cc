a7cb1e731db8ba65c1ad40c7440ef1a41fbe96d5
Martin Felis <martin.felis@iwr.uni-heidelberg.de>
2013-11-01 23:16:20 +0100

    using Model::GetBodyId()

--- src/MarkerModel.cc
+++ src/MarkerModel.cc
@@ -36,22 +36,13 @@ bool MarkerModel::loadFromFile(const char* filename) {
 	assert (rbdlModel == NULL);
 	rbdlModel = new RigidBodyDynamics::Model;
 
-	bool verbose = false;
-	typedef map<string, unsigned int> StringIntMap;
-	StringIntMap body_table_id_map;
-
 	LuaTable model_table = LuaTable::fromFile (filename);
 	
 	if (model_table["gravity"].exists()) {
 		rbdlModel->gravity = model_table["gravity"].get<RigidBodyDynamics::Math::Vector3d>();
-
-		if (verbose)
-			cout << "gravity = " << rbdlModel->gravity.transpose() << endl;
 	}
 
 	int frame_count = model_table["frames"].length();
-
-	body_table_id_map["ROOT"] = 0;
 
 	for (int i = 1; i <= frame_count; i++) {
 		if (!model_table["frames"][i]["parent"].exists()) {
@@ -61,27 +52,18 @@ bool MarkerModel::loadFromFile(const char* filename) {
 
 		string body_name = model_table["frames"][i]["name"].getDefault<string>("");
 		string parent_name = model_table["frames"][i]["parent"].get<string>();
-		unsigned int parent_id = body_table_id_map[parent_name];
+		unsigned int parent_id = rbdlModel->GetBodyId(parent_name.c_str());
+		if (parent_id == std::numeric_limits<unsigned int>::max()) {
+			cerr << "Error: could not find parent body with name '" << parent_name << "'!" << endl;
+			abort();
+			return false;
+		}
 
 		SpatialTransform joint_frame = model_table["frames"][i]["joint_frame"].getDefault(SpatialTransform());
 		Joint joint = model_table["frames"][i]["joint"].getDefault(Joint(JointTypeFixed));
 		Body body = model_table["frames"][i]["body"];
 
 		unsigned int body_id = rbdlModel->AddBody (parent_id, joint_frame, joint, body, body_name);
-		body_table_id_map[body_name] = body_id;
-
-		if (verbose) {
-			cout << "==== Added Body ====" << endl;
-			cout << "  body_name  : " << body_name << endl;
-			cout << "  body id    : " << body_id << endl;
-			cout << "  parent_id  : " << parent_id << endl;
-			cout << "  joint dofs : " << joint.mDoFCount << endl;
-			for (unsigned int j = 0; j < joint.mDoFCount; j++) {
-				cout << "    " << j << ": " << joint.mJointAxes[j].transpose() << endl;
-			}
-			cout << "  joint_frame: " << joint_frame << endl;
-			cout << "  body inertia: " << endl << body.mSpatialInertia << endl;
-		}
 	}
 
 	return true;
