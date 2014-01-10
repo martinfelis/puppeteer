4974708bedb39ca6673e45183fc707c01f922cd0
Martin Felis <martin.felis@iwr.uni-heidelberg.de>
2014-01-11 00:32:42 +0100

    added sd_min, sd_max, and sd_sca generation

--- animation_to_nodedata.py
+++ animation_to_nodedata.py
@@ -9,7 +9,7 @@ frame_first = 239
 frame_rate = 100
 
 phases = [
-        { 'name': 'RightFlat', 'start_frame': 303, 'nshoot': 30},
+        { 'name': 'RightFlat', 'start_frame': 303, 'nshoot': 10},
         { 'name': 'RightToe', 'start_frame': 333, 'nshoot': 10},
         { 'name': 'RightToeTouchDownLeftHeel', 'start_frame': 343, 'nshoot': 1},
         { 'name': 'RightToeLeftHeel', 'start_frame': 343, 'nshoot': 5},
@@ -17,7 +17,7 @@ phases = [
         { 'name': 'RightToeLeftFlat', 'start_frame': 350, 'nshoot': 5},
         { 'name': 'End', 'start_frame': 360}
         ]
-    
+
 animation_data = numpy.genfromtxt("animation.csv", delimiter=',')
 
 dof_names = [
@@ -57,6 +57,13 @@ def calc_interpolated_values (data, timestamp):
 def calc_state_derivatives (data, timestamp, step):
     return (calc_interpolated_values (data, timestamp + step) - calc_interpolated_values (data, timestamp - step)) / (2 * step)
 
+def get_phase_lines (phase_index):
+    assert (phase_index >= 0 and phase_index < len(phases))
+    start_line = phases[phase_index]["start_frame"] - frame_first
+    end_line = phases[phase_index + 1]["start_frame"] - frame_first
+
+    return start_line, end_line
+
 def print_phase (data, phase_index, time_start, time_end, num_nodes):
     print("*\n* Phase: " + str(phase_index) + " " + phases[phase_index]["name"] + "\n*")
     for i in range(0,num_nodes):
@@ -77,7 +84,6 @@ def print_phase (data, phase_index, time_start, time_end, num_nodes):
 
         for j in range(0, len(values_dt)):
             print (str(j + len(values)) + ": " + str(values_dt[j]))
-
         
         print ("")
 
@@ -89,7 +95,40 @@ def print_node_data ():
         phase_start = (phases[i]["start_frame"] - float(frame_first)) / frame_rate
         phase_end = (phases[i + 1]["start_frame"] - float(frame_first)) / frame_rate
 
+        line_start, line_end = get_phase_lines (i)
+
         print_phase (animation_data, i, phase_start, phase_end, phases[i]["nshoot"] + 1)
+
+def print_state_boundaries ():
+    assert (phases[len(phases) - 1]["name"] == "End")
+
+    start_line = phases[0]["start_frame"] - frame_first
+    end_line = phases[len(phases) - 1]["start_frame"] - frame_first
+    
+    print("sd_min(*,*)")
+    for i in range(0, len(dof_names)): 
+        scale = math.fabs(animation_data[start_line:end_line,i+1].max() - animation_data[start_line:end_line,i+1].min())
+        print (str(i) + ": " + str(animation_data[start_line:end_line,i+1].min() - scale * 0.15 ))
+    for i in range(0, len(dof_names)): 
+        print (str(i + len(dof_names)) + ": -10.")
+    print ("")
+
+    print("sd_max(*,*)")
+    for i in range(0, len(dof_names)): 
+        scale = math.fabs(animation_data[start_line:end_line,i+1].max() - animation_data[start_line:end_line,i+1].min())
+        print (str(i) + ": " + str(animation_data[start_line:end_line,i+1].max() + scale * 0.15))
+    for i in range(0, len(dof_names)): 
+        print (str(i + len(dof_names)) + ": 10.")
+
+    print ("")
+
+    print("sd_sca(*,*)")
+    for i in range(0, len(dof_names)): 
+        print (str(i) + ": " + str( math.fabs(animation_data[start_line:end_line,i+1].max() - animation_data[start_line:end_line,i+1].min()) ))
+    for i in range(0, len(dof_names)): 
+        print (str(i + len(dof_names)) + ": 5.")
+
+    print ("")
 
 def print_phase_durations ():
     h = calc_phase_durations(animation_data)
@@ -113,15 +152,22 @@ def print_nshoot():
 def print_xdname():
     print ("xd_name")
     for i in range (0, len(dof_names)):
-        print (str(i) + ": " + dof_names[i])
+        if i == 0:
+            print (str(i) + ": >" + dof_names[i])
+        else:
+            print (str(i) + ": " + dof_names[i])
     for i in range (0, len(dof_names)):
-        print (str(i + len(dof_names)) + ": " + dof_names[i] + "_dot" )
+        print (str(i + len(dof_names)) + ": !" + dof_names[i] + "_dot" )
     print ("")
 
 def print_uname():
     print ("u_name")
     for i in range (6, len(dof_names)):
-        print (str(i - 6) + ": " + dof_names[i - 6])
+        if i == 0:
+            print (str(i - 6) + ": >" + dof_names[i - 6])
+        else:
+            print (str(i - 6) + ": " + dof_names[i - 6])
+    print ("")
 
 # shift the data such that the pelvis origin has x,y coordinates 0,0
 start_line = phases[0]["start_frame"] - frame_first
@@ -132,6 +178,7 @@ print ("* GENERATED INITIAL START")
 print_nshoot()
 print_phase_durations()
 print_node_data()
+print_state_boundaries()
 print_xdname()
 print_uname()
 
